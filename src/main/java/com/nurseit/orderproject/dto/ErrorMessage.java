8e32628acd9399ad5ef19a98cee38d2295b569de
sNurseit <nurseit.taimas@gmail.com>
2025-01-24 21:18:27 +0500

    add exception handler

---
+++ src/main/java/com/nurseit/orderproject/dto/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.nurseit.orderproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class ErrorMessage {
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private String description;
+
+}
